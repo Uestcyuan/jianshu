d8a59ccab33d054a1f2a0934e17950a72f8dd4ff
liyuan <cddzkjly@163.com>
2018-07-23 22:19:42 +0800

    home component demerge

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React, { Component } from "react";
+import { Provider } from "react-redux";
+import { BrowserRouter, Route } from "react-router-dom";
 import Header from "./common/header";
-import store from './store';
-import { Provider } from 'react-redux';
+import Home from "./pages/home";
+import Detail from "./pages/detail";
+import store from "./store";
 
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <Header />
+        <div>
+          <Header />
+          <BrowserRouter>
+            <div>
+              {/* /detail 能匹配一下两个，而不是全字匹配，加上 exact 全字匹配 */}
+              {/* /只匹配了第一个 */}
+              <Route path="/" exact component={Home} />
+              <Route path="/detail" exact component={Detail} />
+            </div>
+          </BrowserRouter>
+        </div>
       </Provider>
     );
   }
