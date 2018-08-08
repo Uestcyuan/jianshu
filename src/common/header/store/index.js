b1ba0567f54170330e2a89e9bc720ac193507033
liyuan <cddzkjly@163.com>
2018-08-08 22:30:41 +0800

    finish every

--- src/common/header/store/index.js
+++ src/common/header/store/index.js
@@ -3,5 +3,3 @@ import * as actionCreator from './actionCreator';
 import * as actionType from './actionType';
 
 export { reducer, actionCreator, actionType };
-
-//这个index.js是header目录的出口文件，引入这里的文件全通过index.js引入
